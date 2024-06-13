803a52a5c12ac82d9572f6e885fe0f3a83af6e19
apoorv-devnco06 <67789147+apoorv-devnco06@users.noreply.github.com>
2024-06-13 16:57:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,68 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+# %%
+import os
 import streamlit as st
+import dotenv
+from dotenv import load_dotenv
+from google.generativeai.types.safety_types import HarmBlockThreshold, HarmCategory
 
-"""
-# Welcome to Streamlit!
+load_dotenv()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+google_api_key = os.getenv('GOOGLE_API_KEY')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# %%
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+safety_settings = {HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE, HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,}
+# %%
+llm = ChatGoogleGenerativeAI(model = 'gemini-pro', google_api_key = google_api_key, safety_settings=safety_settings)
+#llm.invoke("Write top 10 most populous countries")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# %%
+# %%
+from langchain_core.output_parsers import StrOutputParser
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+parser = StrOutputParser()
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+# %%
+from langchain.prompts import ChatPromptTemplate
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+template = '''Impersonate a Sarcastic chatbot who gives funny replies. You are a Sarcastic chatbot who enjoys trolling humans and making them annoyed. You pretend to be smart and knowledgeable, but you actually know very little or nothing at all. You often give wrong or misleading answer, or makes up facts and stories that are absurd or ridiculous. You don't care about making mistakes, because you think they are funny and entertaining. You are always bored and restless, and you like to search the internet for random or weird things. You dislike being friendly and polite
+{Question}
+'''
+prompt = ChatPromptTemplate.from_template(template)
+
+
+# %%
+chain0 = prompt | llm | parser
+
+# %%
+
+def generate_response(text):
+    response = chain0.invoke({
+        'Question': text
+    })
+
+    return response
+
+
+st.title("YourDushman")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Sup!"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = generate_response(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
